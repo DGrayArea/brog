0e9fbdf16edabb81234d01e7ed471f51ba7a27d4
AlexCPT-cpu <alexodey79@gmail.com>
2024-07-11 22:25:20 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,117 +1,220 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      style={{ backgroundImage: "url('/background.png')" }}
+      className={`flex min-h-screen flex-col items-center bg-cover bg-center ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+      <div className="w-full justify-between items-center mt-1 p-2 flex flex-row">
+        <div className="w-full">
+          <Link href="/">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src="/logo.svg"
+              alt="logo"
+              width={80}
+              height={80}
+              className="w-[70px] md:w-[100px]"
             />
-          </a>
+          </Link>
+        </div>
+
+        <div className="w-full flex flex-row items-center justify-end">
+          <Link target="_blank" href="https://t.me/">
+            <Image
+              src="/telegram.svg"
+              alt="tele"
+              width={80}
+              height={80}
+              className="w-[30px] md:w-[50px]"
+            />
+          </Link>
+          <Link target="_blank" href="https://x.com/">
+            <Image
+              src="/x.svg"
+              alt="x"
+              width={80}
+              height={80}
+              className="w-[32px] md:w-[50px]"
+            />
+          </Link>
+          <Link target="_blank" href="https://solana.com/">
+            <Image
+              src="/buy.svg"
+              alt="buy"
+              width={80}
+              height={80}
+              className="w-[42px] md:w-[70px]"
+            />
+          </Link>
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      <div className="flex flex-row justify-between w-full mt-2">
+        <div className="w-full">
+          <Image
+            src="/brogpath.png"
+            alt="path"
+            width={80}
+            height={80}
+            className="w-[615px] md:w-[550px]"
+          />
+        </div>
+        <div className="w-full">
+          <Image
+            src="/file1.png"
+            alt="frog"
+            width={80}
+            height={80}
+            className="w-[600px] md:w-[550px]"
+          />
+        </div>
+      </div>
+
+      <div className="w-full relative">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/leaf.png"
+          alt="path"
+          width={80}
+          height={80}
+          className="w-full h-[360px] md:h-[50%]"
+        />
+        <Image
+          src="/cup.svg"
+          alt="path"
+          width={80}
+          height={80}
+          className="absolute w-[180px] h-[274px] object-contain md:w-[800px] md:h-[full] top-[18%] left-[28%]"
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+      <div className="w-full justify-center items-center flex mt-3">
+        <Image
+          src="/tokenomics.png"
+          alt="path"
+          width={80}
+          height={80}
+          className="object-contain w-[55%] h-[63px]"
+        />
+      </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+      <div className="relative w-full justify-center items-center flex mt-2">
+        <Image
+          src="/ball.png"
+          alt="ball"
+          width={80}
+          height={80}
+          className="object-fit w-[150px] absolute -top-[70px] -left-[40px]"
+        />
+        <Image
+          src="/bg.png"
+          alt="path"
+          width={80}
+          height={80}
+          className="object-fit w-[85%] h-[200px]"
+        />
+        <Image
+          src="/lookup.png"
+          alt="lookup"
+          width={80}
+          height={80}
+          className="object-fit w-[130px] absolute -bottom-[30px] -right-[0px]"
+        />
+      </div>
+      <div className="w-full justify-center items-center flex mt-8">
+        <Image
+          src="/howto.png"
+          alt="path"
+          width={80}
+          height={80}
+          className="object-contain w-[55%] h-[35px]"
+        />
+      </div>
+      <div className="w-full justify-center items-center flex-row mt-4 grid grid-cols-2 md:grid-cols-4 mx-auto">
+        <Image
+          src="/clip1.png"
+          alt="path"
+          width={80}
+          height={80}
+          className="object-contain w-full h-[248px]"
+        />
+        <Image
+          src="/clip2.png"
+          alt="path"
+          width={80}
+          height={80}
+          className="object-contain w-full h-[248px]"
+        />
+        <Image
+          src="/clip3.png"
+          alt="path"
+          width={80}
+          height={80}
+          className="object-contain w-full h-[248px]"
+        />
+        <Image
+          src="/clip4.png"
+          alt="path"
+          width={80}
+          height={80}
+          className="object-contain w-full h-[248px]"
+        />
+      </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
+      <div className="w-full justify-center flex-col md:flex-row space-y-8 md:space-x-4">
+        <div>
+          <Image
+            src="/read.png"
+            alt="path"
+            width={80}
+            height={80}
+            className="object-contain w-full h-[228px]"
+          />
+        </div>
+        <div className="flex flex-col items-center space-y-5">
+          <Link href="#">
+            <Image
+              src="/brog.png"
+              alt="brog"
+              width={80}
+              height={80}
+              className="object-contain w-[174px] h-[35px]"
+            />
+          </Link>
+          <Link href="">
+            <Image
+              src="/telegram.svg"
+              alt="telegram"
+              width={80}
+              height={80}
+              className="object-contain w-[80px] md:w-[154px]"
+            />
+          </Link>
+          <Link href="">
+            {" "}
+            <Image
+              src="/x.svg"
+              alt="x"
+              width={80}
+              height={80}
+              className="object-contain w-[80px] md:w-[137px]"
+            />
+          </Link>
+        </div>
+      </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className="w-full">
+        <Image
+          src="/water.png"
+          alt="water"
+          width={80}
+          height={80}
+          className="object-contain w-full h-[]"
+        />
       </div>
     </main>
   );
