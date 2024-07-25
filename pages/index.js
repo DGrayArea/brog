3a586fa90a2e03666b4c43b9856f43fcc8fda77b
AlexCPT-cpu <alexodey79@gmail.com>
2024-07-25 21:28:55 +0100

    mm

--- pages/index.js
+++ pages/index.js
@@ -52,7 +52,10 @@ export default function Home() {
               className="w-[42px] lg:w-[55px]"
             />
           </Link>
-          <Link target="_blank" href="#">
+          <Link
+            target="_blank"
+            href="https://www.basejump.pro/viewpresale/?tokenAddress=0x2969883334EdB9fdE27C969034C446097bc162c5"
+          >
             <Image
               src="/file (3).png"
               alt="buy"
@@ -242,6 +245,128 @@ export default function Home() {
           </div>
         </div>
       </div>
+      {/* <div className="w-full justify-center items-center flex flex-col md:flex-row mt-12 md:grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 mx-auto patrick">
+        <div className="relative">
+          <ResponsiveImage
+            src="/assets art 1.png"
+            alt="assets"
+            width={1620}
+            height={780}
+            layout="intrinsic"
+            className="object-fit w-[100%] h-auto mx-auto"
+          />
+          <div className="absolute top-[130px] md:top-[262px] lg:top-[132px] xl:top-[165px] md:left-[125px] left-[30px] lg:left-[40px] xl:left-[57px] flex flex-col text-center justify-center items-center space-y-1 md:space-y-1">
+            <div>
+              <ResponsiveImage
+                src="/metamask.svg"
+                alt="assets"
+                width={500}
+                height={500}
+                layout=""
+                className="object-fit w-[17%] md:w-[25%] h-auto mx-auto"
+              />
+            </div>
+            <div className="text-lg md:text-3xl lg:text-xl xl:text-2xl">
+              DOWNLOAD METAMASK
+            </div>
+            <div className="text-xs md:text-lg lg:text-sm xl:text-base w-[140px] md:w-[220px] lg:w-[180px] xl:w-[240px]">
+              Install the MetaMask browser extension or mobile app to create a
+              digital wallet for securely storing your cryptocurrencies and
+              interacting with blockchain applications.
+            </div>
+          </div>
+        </div>
+        <div className="relative mt-5 md:mt-0">
+          <ResponsiveImage
+            src="/assets art 2.png"
+            alt="assets"
+            width={1620}
+            height={780}
+            layout="intrinsic"
+            className="object-fit w-[150%] h-auto mx-auto"
+          />
+          <div className="absolute top-[25px] md:top-[42px] lg:top-[20px] xl:top-[28px] md:left-[125px] left-[90px] lg:left-[55px] xl:left-[75px] flex flex-col text-center justify-center items-center space-y-0 md:space-y-1">
+            <div>
+              <ResponsiveImage
+                src="/eth.png"
+                alt="assets"
+                width={500}
+                height={500}
+                layout=""
+                className="object-fit w-[18%] h-auto mx-auto"
+              />
+            </div>
+            <div className="text-lg md:text-3xl lg:text-xl xl:text-2xl">
+              SECURE ETHEREUM (ETH)
+            </div>
+            <div className="text-xs md:text-lg lg:text-sm xl:text-base w-[140px] md:w-[220px] lg:w-[180px] xl:w-[240px]">
+              Purchase Ethereum (ETH), the foundational currency required for
+              transactions on the BASE network where $BROG operates.
+            </div>
+          </div>
+        </div>
+
+        <div className="relative">
+          <ResponsiveImage
+            src="/assets art 3.png"
+            alt="assets"
+            width={1620}
+            height={780}
+            layout="intrinsic"
+            className="object-fit w-[100%] md:w-[86%] h-auto mx-auto"
+          />
+          <div className="absolute top-[140px] md:top-[42px] lg:top-[130px] xl:top-[165px] md:left-[85px] left-[50px] lg:left-[55px] xl:left-[70px] flex flex-col text-center justify-center items-center space-y-0 md:space-y-1">
+            <div>
+              <ResponsiveImage
+                src="/art assets 2.png"
+                alt="assets"
+                width={500}
+                height={500}
+                layout=""
+                className="object-fit w-[21%] md:w-[32%]h-auto mx-auto"
+              />
+            </div>
+            <div className="text-lg md:text-3xl lg:text-xl xl:text-3xl">
+              BUY $BROG
+            </div>
+            <div className="text-xs md:text-lg lg:text-sm xl:text-base w-[140px] md:w-[220px] lg:w-[180px] xl:w-[240px]">
+              Use your newly acquired ETH base to purchase $BROG tokens through
+              a cryptocurrency exchange or decentralized exchange platform that
+              lists $BROG.
+            </div>
+          </div>
+        </div>
+        <div className="relative mt-5 md:mt-0">
+          <ResponsiveImage
+            src="/assets art 4.png"
+            alt="assets"
+            width={500}
+            height={500}
+            layout=""
+            className="object-fit w-[100%] h-auto mx-auto"
+          />
+          <div className="absolute top-[24px] md:top-[42px] lg:top-[25px] md:left-[85px] -left-[50px] lg:-left-[45px] xl:-left-[50px] flex flex-col text-center justify-center items-center space-y-0 md:space-y-1">
+            <div>
+              <ResponsiveImage
+                src="/wallet.svg"
+                alt="assets"
+                width={500}
+                height={500}
+                layout=""
+                className="object-fit w-[13%] md:w-[15%] h-auto mx-auto"
+              />
+            </div>
+            <div className="text-lg md:text-3xl lg:text-xl xl:text-3xl">
+              BUY $BROG
+            </div>
+            <div className="text-xs md:text-lg lg:text-sm xl:text-base w-[140px] md:w-[220px] lg:w-[180px] xl:w-[240px]">
+              Import $BROG tokens into your MetaMask wallet by adding the
+              token&apos;s contract address. This allows you to view and manage
+              your holdings directly.
+            </div>
+          </div>
+        </div>
+      </div> */}
 
       <div className="w-full flex justify-center items-center flex-col lg:flex-row lg:justify-around space-y-8 lg:space-x-4 my-4 lg:mt-20">
         <div className="mt-10">
